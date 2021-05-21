3470fe7d1065dc26706514346391ed7806fbe3b3
rilith <bludoftenchiefs@protonmail.com>
2021-05-20 19:35:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    cool()
+})
+    function cool() {
+        document.getElementById('text').textContent = 'This is really cool!';
+    }
+
+
