a0174bb97c0893cd4b9cfa9d5eba0986c379f662
rilith <bludoftenchiefs@protonmail.com>
2021-05-20 19:26:06 -0500

    commit tree 256a396ce25aec8175fca08c35dcf0b0e096ba93

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    cool()
+})
+    function cool() {
+        document.getElementById('text').textContent = 'This is really cool!';
+    }
+
+
+
